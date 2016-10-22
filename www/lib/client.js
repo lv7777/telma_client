8760d96b5d10710675a95ec48abfc824c05454f4
lv7777 <oonuma.ry0uyu@gmail.com>
2016-10-23 07:32:30 +0900

    chat.jsの処理をclient.jsにまとめた

--- www/lib/client.js
+++ www/lib/client.js
@@ -116,3 +116,21 @@ function fakeMessage(msg) {
 }
 
 
+
+function chat_init(p_uid=22){
+  $.ajax({
+   type: "GET",
+   url:"https://it2-sotuken.herokuapp.com/chat_init?partner_user_id="+p_uid,
+   success: function(data){
+       console.log(data)
+       //TODO:icon
+       
+       //name
+       $(".chat-title #userName").html(data[0].fullname);
+    },
+    error: function(err){
+      console.log("ajax-error!<br>"+JSON.stringify(err));
+    }
+     });
+}
+
