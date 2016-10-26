d043a2504b6a37b47351135dc43a814633e9b28e
lv7777 <oonuma.ry0uyu@gmail.com>
2016-10-27 02:23:12 +0900

    貸し借り用のリストにapiを投げてそのデータを表示させるようにできた。

--- www/js/rent.js
+++ www/js/rent.js
@@ -55,3 +55,36 @@ function rentWrite(){
 }
 
 /*******************/
+
+//記事リスト
+//list-allする。最初の一回で呼び出すだけ。
+function listfirst(){
+    //allclear();
+  $("#load-dialog").show();
+  console.log("listfirst in rent");
+  var sendData = {
+    ido:"300",keido:"300"
+  };
+  $.ajax({
+   type: "get",
+   url:"https://it2-sotuken.herokuapp.com/kashikari/list-all",
+   data:sendData,
+   success: function(msg){
+        
+    console.log("success!");
+    console.log(JSON.stringify(msg));
+//     $.each(msg,function(key,val){
+//       var listDom = $(".bbs-list-seed:first").clone(true);//.html("List"+listCnt)
+//       listDom.attr("onclick","bbsDetail("+val.keiji_id+")");
+//       listDom.find(".list__item__title").html(val.title);
+//       listDom.find(".list__item__subtitle").html(val.content);
+//        listDom.prependTo($("#bbs-lists"));
+// 
+//     });
+    },
+    error: function(err){
+      console.log("ajax-error!<br>"+JSON.stringify(err));
+    }
+     });
+  /******/
+}
